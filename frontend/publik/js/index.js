c54232ecb67eb4b6093fe067d7ea31f239d4807e
username-yogadf7-spec <email-yogadf7@gmail.com>
2026-08-18 10:26:37 +0700

    pembaruan publik halaman index dan ppid

--- frontend/publik/js/index.js
+++ frontend/publik/js/index.js
@@ -365,6 +365,27 @@ btnProduk.addEventListener("mouseleave", () => {
   btnProduk.classList.remove("pulse-button");
 });
 
+// ======================
+// SCROLL TO TOP
+// ======================
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add("show");
+  } else {
+    scrollTopBtn.classList.remove("show");
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
 // =======================
 // CARD STATISTIK
 // ======================
